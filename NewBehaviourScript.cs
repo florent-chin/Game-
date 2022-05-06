f0d0f5b2ab40207b7f4f859855e65f40219115bc
florent-chin <huupmasari@gmail.com>
2022-05-06 21:23:46 +0900

    3th-commit

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -13,6 +13,10 @@ public class NewBehaviourScript : MonoBehaviour
     void Create(){
         
     }
+
+    void Delete(){
+        
+    }
     // Update is called once per frame
     void Update()
     {
